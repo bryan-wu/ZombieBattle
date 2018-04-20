e149ec04248b1937a575701ab0a7f38946a83dc7
Bryan Wu <bryanwu@umail.ucsb.edu>
2018-04-19 17:35:42 -0700

    Added ZombieBattle version with refactored package and class names.

--- core/src/com/zombiebattle/game/ZombieBattle.java
+++ core/src/com/zombiebattle/game/ZombieBattle.java
@@ -1,4 +1,4 @@
-package com.mygdx.game;
+package com.zombiebattle.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -6,14 +6,14 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class ZombieBattle extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("Zombie Battle.png");
+		img = new Texture("zombie_battle_logo.png");
 	}
 
 	@Override
